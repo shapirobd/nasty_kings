c5dd621e2ff09edce3c2776496a26e3c518f5eea
Brian Shapiro <briandavidshapiro@gmail.com>
2022-03-21 18:55:49 -0400

    more footer margin

--- src/componentsMobile/Footer/styles/FooterStyles.js
+++ src/componentsMobile/Footer/styles/FooterStyles.js
@@ -8,7 +8,7 @@ export const useStyles = makeStyles((theme) => ({
 		// bottom: "0%",
 		display: "flex",
 		justifyContent: "center",
-		margin: "30px 0",
+		margin: "70px 0",
 	},
 	innerDiv: {
 		width: "90%",
