6e6ea37151254bff75dfbce1a86cd3ba6079acb1
Brian Shapiro <briandavidshapiro@gmail.com>
2022-03-20 23:25:35 -0400

    fixing super small line

--- src/AppStyles.js
+++ src/AppStyles.js
@@ -11,7 +11,7 @@ export const useStyles = makeStyles((theme) => ({
 	mobileApp: {
 		textAlign: "center",
 		backgroundColor: "#000",
-		height: "90vh",
+		height: "90.3vh",
 		overflow: "auto",
 		position: "relative",
 	},
