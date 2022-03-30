39d29361199bb9ade18a397833f10946b640b7f1
Brian Shapiro <briandavidshapiro@gmail.com>
2022-03-29 20:33:10 -0400

    reducing bottom margin on footer

--- src/componentsMobile/Footer/styles/FooterStyles.js
+++ src/componentsMobile/Footer/styles/FooterStyles.js
@@ -8,7 +8,7 @@ export const useStyles = makeStyles((theme) => ({
 		// bottom: "0%",
 		display: "flex",
 		justifyContent: "center",
-		margin: "30px 0 90px",
+		margin: "30px 0 30px",
 	},
 	innerDiv: {
 		width: "90%",
