06f12689476472be42de01c0a19e1df38c370739
Brian Shapiro <briandavidshapiro@gmail.com>
2023-04-28 00:20:25 -0400

    more cleanup

--- src/components/Shows/ShowsPage.js
+++ src/components/Shows/ShowsPage.js
@@ -113,34 +113,6 @@ const ShowsPage = ({ fromHome = false }) => {
 		return { venue, address, city, state, date, time, venueLink, ticketLink, solo, prevShow };
 	}
 
-	// const shows = [
-	// 	createData(
-	// 		"Nash Street Tavern",
-	// 		"250 S. Nash Sreet Hillsborough, NC",
-	// 		{ month: "May", day: "26", year: "2022", weekDay: "THURSDAY" },
-	// 		"6:00 PM",
-	// 		"https://www.facebook.com/NashStreetTavern/",
-	// 		"Free",
-	// 		true
-	// 	),
-	// 	createData(
-	// 		"Highland Brewing",
-	// 		"12 Old Charlotte Highway, Suite 200 Asheville, NC",
-	// 		{ month: "June", day: "11", year: "2022", weekDay: "SATURDAY" },
-	// 		"6:00 PM",
-	// 		"https://highlandbrewing.com/",
-	// 		"Free"
-	// 	),
-	// 	createData(
-	// 		"Bullhorn Fest",
-	// 		"Shadowbox Studio, 2200-D Dominion St. Durham, NC",
-	// 		{ month: "September", day: "24", year: "2022", weekDay: "SATURDAY" },
-	// 		"",
-	// 		"https://bullhornarts.org/",
-	// 		"Free"
-	// 	),
-	// ];
-
 	const [shows, setShows] = useState([]);
 
 	useEffect(async () => {
